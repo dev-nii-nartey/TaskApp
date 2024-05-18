38bc677e5c7a9fc654af35fe03c96f476273bebf
dev-nii-nartey <ictkemba@gmail.com>
2024-05-18 20:47:38 +0000

    add: methods to handle requests to  delete task and get a single task by task id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { CreateTaskData } from './dto/create-task.dto';
+import { CreateTaskDto, TaskIdDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -12,9 +12,17 @@ export class TasksController {
   }
 
   @Post()
-  createTask(@Body() createtaskdata: CreateTaskData) {
-    const { title, description } = createtaskdata;
-    return this.tasksService.createTask(title, description);
+  createTask(@Body() createtaskdto: CreateTaskDto) {
+    return this.tasksService.createTask(createtaskdto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param() taskiddto: TaskIdDto) {
+    return this.tasksService.deleteTask(taskiddto);
+  }
+
+  @Post('/:id')
+  getTaskById(@Param() taskdto: TaskIdDto) {
+    return this.tasksService.getTask(taskdto);
   }
 }
-
