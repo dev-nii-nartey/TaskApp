a98e858a0d7cde6cfb499fc53ecbbc7bbf38820f
dev-nii-nartey <ictkemba@gmail.com>
2024-05-21 13:46:39 +0000

    add: validation pipes to request handlers for the tasks CRUD operations

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,18 +7,21 @@ import {
   Patch,
   Post,
   Query,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto, TaskIdDto } from './dto/create-task.dto';
-import { TASK_STATUS } from './task.model';
+import { TASK_STATUS, Task } from './task.model';
 import { FilterDto } from './dto/filter-task.dto';
+import { TaskFilterValidationPipe } from './pipes/task-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  getTasks(@Query() query: FilterDto) {
+  getTasks(@Query(ValidationPipe) query: FilterDto): Task[] {
     if (Object.keys(query).length) {
       return this.tasksService.filterTasks(query);
     } else {
@@ -27,24 +30,26 @@ export class TasksController {
   }
 
   @Post()
-  createTask(@Body() createtaskdto: CreateTaskDto) {
+  @UsePipes(ValidationPipe)
+  createTask(@Body() createtaskdto: CreateTaskDto): Task {
     return this.tasksService.createTask(createtaskdto);
   }
 
   @Delete('/:id')
-  deleteTask(@Param() taskiddto: TaskIdDto) {
+  deleteTask(@Param() taskiddto: TaskIdDto): void {
     return this.tasksService.deleteTask(taskiddto);
   }
 
   @Post('/:id')
-  getTaskById(@Param() taskdto: TaskIdDto) {
+  getTaskById(@Param() taskdto: TaskIdDto): Task {
     return this.tasksService.getTask(taskdto);
   }
 
   @Patch('/:id')
-  updateTask(@Param() taskdto: TaskIdDto, @Body('status') body: TASK_STATUS) {
-    const element = this.tasksService.getTask(taskdto);
-    return this.tasksService.updateTaskStatus(element, body);
+  updateTask(
+    @Param() taskdto: TaskIdDto,
+    @Body('status', TaskFilterValidationPipe) body: TASK_STATUS,
+  ): Task {
+    return this.tasksService.updateTaskStatus(taskdto, body);
   }
-
 }
