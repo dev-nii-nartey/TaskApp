8e92fec74cafa951dbb8983670574489535c37ed
dev-nii-nartey <ictkemba@gmail.com>
2024-05-21 13:41:23 +0000

    add: validation decorations to dto for filtering and creating new tasks

--- src/tasks/dto/filter-task.dto.ts
+++ src/tasks/dto/filter-task.dto.ts
@@ -1,6 +1,12 @@
+import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 import { TASK_STATUS } from '../task.model';
 
 export class FilterDto {
-  status?: TASK_STATUS;
-  search?: string;
+  @IsOptional()
+  @IsIn([TASK_STATUS.DONE, TASK_STATUS.OPEN, TASK_STATUS.PROGRESS])
+  status: TASK_STATUS;
+
+  @IsOptional()
+  @IsNotEmpty()
+  search: string;
 }
